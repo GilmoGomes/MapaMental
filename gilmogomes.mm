7130a3ff853e5841f0d30e4404158f4d51f11b3c
GilmoGomes <68614445+GilmoGomes@users.noreply.github.com>
2022-02-07 13:24:51 -0300

    Add files via upload

---
+++ gilmogomes.mm
@@ -0,0 +1,251 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#00cc33" CREATED="1611796516427" ID="ID_1032899088" MODIFIED="1612183562320" TEXT="Gilmo Gomes">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1611796032698" ID="ID_659073876" MODIFIED="1612183645351" POSITION="left" TEXT="Estudos">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1611796139604" ID="ID_1609715583" MODIFIED="1612180433032" TEXT="UNA">
+<node CREATED="1611796144683" ID="ID_1571031603" MODIFIED="1612180498375">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tecnologo<br />Engenharia de<br />Software
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1612183398590" ID="ID_1590790021" MODIFIED="1612183411606" TEXT="Iterasys"/>
+</node>
+<node CREATED="1611796040517" ID="ID_1043735712" MODIFIED="1612183648166" POSITION="left" TEXT="Familia">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1611796155364" ID="ID_1924019851" MODIFIED="1611796157329" TEXT="Esposa">
+<node CREATED="1611796182586" ID="ID_254417844" MODIFIED="1612180509179" TEXT="Jeaner"/>
+</node>
+<node CREATED="1611796157732" ID="ID_1853861805" MODIFIED="1611796160469" TEXT="Filhos">
+<node CREATED="1611796160471" ID="ID_318476216" MODIFIED="1612180520751" TEXT="2">
+<node CREATED="1611796186035" ID="ID_1895097977" MODIFIED="1612180534398" TEXT="Maria Helena"/>
+<node CREATED="1611796188446" ID="ID_470156164" MODIFIED="1612180543873" TEXT="Jo&#xe3;o Pedro"/>
+</node>
+</node>
+<node CREATED="1611796170577" ID="ID_259358566" MODIFIED="1611796181799" TEXT="Pet">
+<node CREATED="1611796204773" ID="ID_1231969400" MODIFIED="1612180565786" TEXT="Symo"/>
+</node>
+</node>
+<node CREATED="1611796105426" ID="ID_1155117042" MODIFIED="1612183698487" POSITION="right" TEXT="Hobbies">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1611796435085" ID="ID_1439364291" MODIFIED="1612180606490" TEXT="Futebol">
+<node BACKGROUND_COLOR="#66cc00" CREATED="1612183032456" ID="ID_693714823" MODIFIED="1612183603722">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sou torcerdor do
+    </p>
+    <p>
+      Time Am&#233;rica Mineiro
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1611796441715" ID="ID_1231231309" MODIFIED="1612208018855">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Viajar para qualquer
+    </p>
+    <p>
+      interior de MG
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1611796445260" ID="ID_522950802" MODIFIED="1611796448438" TEXT="Ler">
+<node CREATED="1612183243638" ID="ID_1370581648" MODIFIED="1612183270354" TEXT="O Executivo e o Elefante"/>
+<node CREATED="1612183274798" ID="ID_1636001428" MODIFIED="1612183355578" TEXT="Cole&#xe7;&#xe3;o : Voc&#xea; S/A"/>
+</node>
+<node CREATED="1611796448821" ID="ID_691732217" MODIFIED="1612180624107" TEXT="M&#xfa;sica">
+<node CREATED="1612183160359" ID="ID_523766650" MODIFIED="1612183166575" TEXT="MPB"/>
+<node CREATED="1612183169822" ID="ID_1611146609" MODIFIED="1612183174531" TEXT="Salmos"/>
+<node CREATED="1612183181846" ID="ID_367367108" MODIFIED="1612183194242" TEXT="Cat&#xf3;licas/Gospel"/>
+</node>
+<node CREATED="1611796450919" ID="ID_1043795507" MODIFIED="1612183577610" TEXT="Vilol&#xe3;o">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1612183201071" ID="ID_1933698812" MODIFIED="1612183230523">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tocar/eterno aprendiz
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1611796454026" ID="ID_1543463977" MODIFIED="1611796463726" TEXT="Jogos">
+<node CREATED="1611796463726" ID="ID_486377416" MODIFIED="1612180654659" TEXT="Tabuleiro">
+<node CREATED="1612180657272" ID="ID_464723471" MODIFIED="1612180665548" TEXT="Dama"/>
+</node>
+<node CREATED="1611796478261" ID="ID_434562275" MODIFIED="1612180672193" TEXT="Mesa">
+<node CREATED="1612180674456" ID="ID_1830071820" MODIFIED="1612180684533" TEXT="Sinuca"/>
+<node CREATED="1612180711216" ID="ID_967898912" MODIFIED="1612180717609" TEXT="Futmesa"/>
+</node>
+<node CREATED="1611796485613" ID="ID_460078589" MODIFIED="1612180694420" TEXT="Rede">
+<node CREATED="1612180696105" ID="ID_165079207" MODIFIED="1612180738968" TEXT="Futvolei"/>
+<node CREATED="1612180766265" ID="ID_179539171" MODIFIED="1612180773126" TEXT="Peteca"/>
+</node>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1611796054316" ID="ID_639343648" MODIFIED="1612183691762" POSITION="right" TEXT="Trabalho">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1611796271078" ID="ID_743783754" MODIFIED="1612182757858" TEXT="Atual">
+<node CREATED="1611796242577" ID="ID_1471837692" MODIFIED="1612182877044">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Simply
+    </p>
+    <p>
+      Tecnologia
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1611796253468" ID="ID_12436781" MODIFIED="1612182685422" TEXT="PWA"/>
+<node CREATED="1611796257313" ID="ID_1280019991" MODIFIED="1611796261415" TEXT="Bradesco"/>
+<node CREATED="1612208192894" ID="ID_1770360004" MODIFIED="1612208264680">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Iniciei como
+    </p>
+    <p>
+      suporte h&#237;brido
+    </p>
+    <p>
+      (Suporte/QA)<br />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1612208270734" ID="ID_498541050" MODIFIED="1612208311208">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Atualmente
+    </p>
+    <p>
+      somente
+    </p>
+    <p>
+      Teste/QA
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1611796279564" ID="ID_751208478" MODIFIED="1612182769850" TEXT="Anteriores">
+<node CREATED="1612182883998" ID="ID_904078026" MODIFIED="1612208071152">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Teknisa
+    </p>
+    <p>
+      Sistema ERP
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1612182885142" ID="ID_227487619" MODIFIED="1612182957428">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Totvs
+    </p>
+    <p>
+      Sistema ERP
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1612182886470" ID="ID_1362676237" MODIFIED="1612183011188">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      RM Sistemas
+    </p>
+    <p>
+      Sistema ERP<br />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1611796332774" ID="ID_1200153164" MODIFIED="1612183759809">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Futuros
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1612182835375" ID="ID_441539852" MODIFIED="1612182847306" TEXT="Iterasys">
+<node CREATED="1612183775958" ID="ID_1265239167" MODIFIED="1612183817465">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Contribui&#231;&#227;o
+    </p>
+    <p>
+      em palestras
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1611797486237" ID="ID_22768562" MODIFIED="1612183650912" POSITION="left" TEXT="Geografia">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1611797549559" ID="ID_688364148" MODIFIED="1612183446103" TEXT="Nasci Minas Gerais"/>
+<node CREATED="1611797555504" ID="ID_1981036484" MODIFIED="1612180593364" TEXT="Moro em BH"/>
+</node>
+</node>
+</map>
