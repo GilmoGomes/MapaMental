00448336aaa7c36eb04d907339afc8bd98c364a4
GilmoGomes <68614445+GilmoGomes@users.noreply.github.com>
2022-02-07 13:27:05 -0300

    Add files via upload

---
+++ Planejamento de Testes.mm
@@ -0,0 +1,341 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1608263645555" ID="ID_264200186" MODIFIED="1608270146273" TEXT="Planejamento de Testes">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1608264975252" ID="ID_308644829" MODIFIED="1608270372171" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Plano de
+    </p>
+    <p>
+      Teste
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1608265110971" ID="ID_320941015" MODIFIED="1608270176797">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &quot;Contrato&quot; entre
+    </p>
+    <p>
+      Equipe e
+    </p>
+    <p>
+      o Clietne
+    </p>
+  </body>
+</html></richcontent>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1608265232348" ID="ID_872707114" MODIFIED="1608291183745" TEXT="Escopo">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1608265456484" ID="ID_1483700111" MODIFIED="1608290027139">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      O que vai ser
+    </p>
+    <p>
+      Testado
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1608289931530" ID="ID_896004105" MODIFIED="1608291230991" TEXT="09.06.0006">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1608290039807" ID="ID_1721507523" MODIFIED="1608290111575">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Step-Selecione a
+    </p>
+    <p>
+      Cesta de Servi&#231;o
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608290043068" ID="ID_1101788494" MODIFIED="1608290244273" TEXT="Proposta da Conta"/>
+<node CREATED="1608290044806" ID="ID_945951759" MODIFIED="1608290266340" TEXT="Produto desejado"/>
+<node CREATED="1608290046709" ID="ID_1254762303" MODIFIED="1608290312647" TEXT="Redistribuir valores"/>
+<node CREATED="1608290316685" ID="ID_1813066857" MODIFIED="1608290343959" TEXT="Deseja usar a conta"/>
+</node>
+<node CREATED="1608289934959" ID="ID_1570021754" MODIFIED="1608290018112" TEXT="09.07.0001"/>
+</node>
+<node CREATED="1608265458204" ID="ID_1302882607" MODIFIED="1608265570664">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      O que n&#227;o
+    </p>
+    <p>
+      vai ser Testado
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1608265237284" ID="ID_1280210654" MODIFIED="1608265305135" TEXT="Cronograma">
+<node CREATED="1608265707683" ID="ID_1786856033" MODIFIED="1608265718710" TEXT="Prazos"/>
+<node CREATED="1608265709044" ID="ID_1887122167" MODIFIED="1608265747586">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Quando vai
+    </p>
+    <p>
+      ser Testado
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1608265238540" ID="ID_165086961" MODIFIED="1608265356974" TEXT="Or&#xe7;amento">
+<node CREATED="1608265814612" ID="ID_610085243" MODIFIED="1608265833070" TEXT="Quanto vai custar"/>
+<node CREATED="1608265818060" ID="ID_772118372" MODIFIED="1608266084589" TEXT="Recursos"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1608265239756" ID="ID_853398740" MODIFIED="1608289865864" TEXT="Qualidade">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1608266139683" ID="ID_512710862" MODIFIED="1608266151357" TEXT="Indicadores">
+<node CREATED="1608266183827" ID="ID_880983858" MODIFIED="1608266225493" TEXT="Quant.de Bugs"/>
+<node CREATED="1608266185018" ID="ID_1542803928" MODIFIED="1608266363223">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Quant. Bugs
+    </p>
+    <p>
+      por step
+    </p>
+    <p>
+      do PWA(Front)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608266186283" ID="ID_612434001" MODIFIED="1608266500367">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tend&#234;ncia de
+    </p>
+    <p>
+      aumento de Bugs
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608266187850" ID="ID_490793810" MODIFIED="1608266489960">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tend&#234;ncia de
+    </p>
+    <p>
+      Redu&#231;&#227;o de
+    </p>
+    <p>
+      Bugs
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608266531914" ID="ID_66208380" MODIFIED="1608266560511">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tempo de
+    </p>
+    <p>
+      encontrar um Bug
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608266566650" ID="ID_196981406" MODIFIED="1608266593129">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tempo de
+    </p>
+    <p>
+      Solucionar
+    </p>
+    <p>
+      um Bug
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1608266140771" ID="ID_1953271197" MODIFIED="1608268165049" TEXT="Metas">
+<node CREATED="1608267492017" ID="ID_1286061825" MODIFIED="1608267588085">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Exemplo
+    </p>
+    <p>
+      (5 bugs por est&#243;rias de usuarios)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608267495129" ID="ID_1384254010" MODIFIED="1608267784285">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Objetivo(reduzir
+    </p>
+    <p>
+      bugs por
+    </p>
+    <p>
+      estorias de usuarios)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608268165049" ID="ID_744916657" MODIFIED="1608268172363" TEXT="Treinamento"/>
+<node CREATED="1608268192784" ID="ID_1007906931" MODIFIED="1608268228734">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Melhorar a forma
+    </p>
+    <p>
+      de testar
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1608265240892" ID="ID_1038810286" MODIFIED="1608265384782" TEXT="Risco">
+<node CREATED="1608268352880" ID="ID_806308377" MODIFIED="1608268410598">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      O que pode
+    </p>
+    <p>
+      dar errado
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1608268423360" ID="ID_751027398" MODIFIED="1608268676304" TEXT="No projeto"/>
+<node CREATED="1608268424721" ID="ID_233973471" MODIFIED="1608268681506" TEXT="No Produto"/>
+</node>
+</node>
+<node CREATED="1608265242044" ID="ID_65806274" MODIFIED="1608265393344" TEXT="Pessoas">
+<node CREATED="1608268885280" ID="ID_1184422392" MODIFIED="1608268897854" TEXT="Devs"/>
+<node CREATED="1608268887840" ID="ID_328713426" MODIFIED="1608268902332" TEXT="ANs"/>
+<node CREATED="1608268889382" ID="ID_773227751" MODIFIED="1608268905474" TEXT="QA"/>
+</node>
+<node CREATED="1608265397397" ID="ID_1176688823" MODIFIED="1608265407894" TEXT="Aquisi&#xe7;&#xf5;es">
+<node CREATED="1608269038304" ID="ID_1031440633" MODIFIED="1608269062586" TEXT="Ferramentas de Softwares"/>
+<node CREATED="1608269086583" ID="ID_443254536" MODIFIED="1608269109515" TEXT="Ler contratos "/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1608265426124" ID="ID_741592906" MODIFIED="1608270219405" TEXT="Comunica&#xe7;&#xe3;o">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1608269145296" ID="ID_1127907006" MODIFIED="1608269243073" TEXT="Como Comunicar"/>
+<node CREATED="1608269146727" ID="ID_1948252437" MODIFIED="1608270223120" TEXT="Quem Comunicar">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1608269147711" ID="ID_1930874490" MODIFIED="1608269277562" TEXT="O que Comunicar"/>
+<node CREATED="1608269148599" ID="ID_1245774350" MODIFIED="1608269220481" TEXT="Quando Comunicar"/>
+<node CREATED="1608269154471" ID="ID_318431029" MODIFIED="1608269154471" TEXT=""/>
+</node>
+</node>
+</node>
+<node CREATED="1608265082212" ID="ID_697517350" MODIFIED="1608269505497" POSITION="left" TEXT="IEC/ISO 29119-3">
+<node CREATED="1608269508518" ID="ID_1633675748" MODIFIED="1608269550796">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Padr&#227;o de
+    </p>
+    <p>
+      Documenta&#231;&#227;o de Teste
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1608269611463" ID="ID_50521613" MODIFIED="1608269620114" POSITION="left" TEXT="Mapa Mental">
+<node CREATED="1608269622031" ID="ID_1232201818" MODIFIED="1608269645202" TEXT="Simples e R&#xe1;pido"/>
+<node CREATED="1608269650943" ID="ID_721460268" MODIFIED="1608269713562" TEXT="Mapear os Steps">
+<node CREATED="1608269715790" ID="ID_1735092192" MODIFIED="1608269761004" TEXT="A ser desenvolvido"/>
+<node CREATED="1608269724030" ID="ID_1990800590" MODIFIED="1608269767322" TEXT="A ser testato"/>
+<node CREATED="1608269724886" ID="ID_613379420" MODIFIED="1608269779482" TEXT="Necessidades"/>
+<node CREATED="1608269725758" ID="ID_1934649440" MODIFIED="1608269788425" TEXT="Restri&#xe7;&#xf5;es"/>
+<node CREATED="1608269726495" ID="ID_1346392742" MODIFIED="1608269891615">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Substituro de um
+    </p>
+    <p>
+      plano de teste tradiconal
+    </p>
+    <p>
+      (Word, Excel etc)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
